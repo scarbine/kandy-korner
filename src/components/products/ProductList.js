ab5b2311662921b5ce736d003916dea613dabc52
Sam Carbine <scarbine@me.com>
2021-07-09 14:17:57 -0500

    added product types to each type of candy

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -3,27 +3,31 @@
 import React, { useContext, useEffect } from "react"
 import { ProductContext } from "./ProductProvider"
 import { ProductCard } from "./ProductCard"
+import { ProductTypeContext} from "../productTypes/ProductTypeProvider"
 import './Product.css'
 
 
 export const ProductList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { products, getProducts } = useContext(ProductContext)
+  const { productTypes, getProductTypes} = useContext(ProductTypeContext)
 
   //useEffect - reach out to the world for something
   useEffect(() => {
     console.log("ProductList: useEffect - getProducts")
-    getProducts()
+    getProductTypes()
+    .then(getProducts)
 
   }, [])
 
 
   return (
     <div className="products">
-      {console.log("ProductList: Render", products)}
+      {console.log("ProductList: Render", products, productTypes)}
       {
         products.map(product => {
-          return <ProductCard key={product.id} product={product} />
+          const productType = productTypes.find(pt => pt.id === product.productTypes.id)
+          return <ProductCard key={product.id} product={product} productType={productType} />
         })
       }
     </div>
